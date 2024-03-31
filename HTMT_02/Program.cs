031e8b043a256b7039c6ddbefb09390d92e6c2d2
KhongMinhThu <165476472+KhongMinhThu@users.noreply.github.com>
2024-03-31 21:41:56 +0700

    Initial commit

---
+++ HTMT_02/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using HTMT_02;
+
+namespace HTMT_02
+{
+    internal class Program
+    {
+        public static void Menu()
+        {
+            Console.WriteLine("*********************  Menu ******************");
+            Console.WriteLine("1. Biểu diễn dãy bit nhị phân của số nguyên N.");
+            Console.WriteLine("2. Số nguyên N của dãy nhị phân A.");
+            Console.WriteLine("3. Thoát");
+            Console.WriteLine();
+
+        }
+        public static void Main(string[] args)
+        {
+            int KQ;
+            int choice = 0;
+            Menu();
+            do
+            {
+                choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        int N;
+                        N = Xulyheso.Nhap("Số nguyên N:");
+                        Xulyheso.Bieudiensonguyenduoidangdaynhiphan(N);
+                        Console.WriteLine();
+                        break;
+
+                    case 2:
+                        int[] A;
+                        A = Xulyheso.Nhapmang();
+                        Console.WriteLine("Dãy nhị phân A: ");
+                        Xulyheso.Xuatmang(A);
+                        KQ = Xulyheso.Chuyensonhiphanthanhsonguyen(A);
+                        Console.WriteLine($"Số nguyên X của dãy nhị phân  là: {KQ}");
+                        Console.WriteLine();
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Thoát");
+                        return;
+
+                } 
+            } while (choice != 3);
+        }
+
+    }
+}
---
+++ HTMT_02/Xulyheso.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+
+namespace HTMT_02
+{
+	public class Xulyheso
+	{
+        public static int[] Nhapmang()
+        {
+            int[] a = new int[32];
+            Console.WriteLine("Nhập dãy nhị phân A của số nguyên : ");
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = int.Parse(Console.ReadLine());
+            }
+            return a;
+        }
+
+        public static int Nhap(string Ghichu)
+        {
+            Console.Write(Ghichu);
+            int a = int.Parse(Console.ReadLine());
+            return a;
+        }
+
+        public static void Xuatmang(int[] a)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write(a[i]);
+            }
+            Console.WriteLine();
+        }
+
+
+        public static void InBit(BitArray A, string X="")
+        {
+            int i = 0;
+            Console.Write("{0}\t", X);
+            foreach (bool Item in A)
+            {
+                i++;
+                if (Item)
+                    Console.Write("1");
+                else
+                    Console.Write("0");
+                if (i % 4 == 0)
+                    Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+
+        public static void Bieudiensonguyenduoidangdaynhiphan(int N)
+        {
+            int[] a = { 2, 3 };
+            BitArray A1 = new BitArray(32);
+            A1 = new BitArray(a);
+            int size = A1.Count;
+            for (int i = 0; i < size / 2; i++)
+            {
+                bool m = A1[i];
+                A1[i] = A1[size - i - 1];
+                A1[size - i - 1] = m;
+            }
+            Console.WriteLine("Dãy nhị phân của số nguyên N: ");
+            InBit(A1);
+        }
+
+
+        public static int Chuyensonhiphanthanhsonguyen(int[] a)
+        {
+            double T = 1;
+            double K = 1;
+            int kq = 0;
+            int n = a.Length;
+            if (a[0] == 1)
+            {
+                K = -1 * a[0] * Math.Pow(2, 31);
+            }
+            if (a[0] == 0)
+            {
+                K = a[0] * Math.Pow(2, 31);
+            }
+            for (int i = 1; i < n; i++)
+            {
+                T = a[i] * Math.Pow(2, n - i - 1);
+                kq = (int)(kq + T);
+            }
+            kq = (int)(kq + K);
+            return kq;
+        }
+    }
+}
+
